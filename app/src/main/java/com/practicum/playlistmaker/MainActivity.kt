3270ba383d1e2686ecc05473027a0e872a31ac0c
NeoGoHan_dev <NeoGoHan1@yandex.ru>
2025-10-19 00:01:55 +0300

    описание

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        findViewById<android.view.View>(R.id.searchButton).setOnClickListener(
+            object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    val intent = Intent(this@MainActivity, SearchActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        )
+
+        findViewById<android.view.View>(R.id.mediaLibraryButton).setOnClickListener {
+            val intent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<android.view.View>(R.id.settingsButton).setOnClickListener {
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<android.widget.ImageView>(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
